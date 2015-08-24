d9cd5401a782313a2e02c81c3bc69f25f8fb1acc
Thorsten Bischler <thorsten.bischler@uni-wuerzburg.de>
2015-08-24 18:31:27 +0200

    Add small helper class to check if float parameters are in allowed range

---
+++ peakachulib/float_range.py
@@ -0,0 +1,6 @@
+class FloatRange(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    def __eq__(self, other):
+        return self.start <= other <= self.end
