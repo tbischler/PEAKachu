5affbdb11d5f5ed40134c84f86646f4477e39cb6
Thorsten Bischler <thorsten.bischler@uni-wuerzburg.de>
2016-03-07 14:14:40 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -8,11 +8,17 @@ setup(
     version='0.1dev',
     packages=['peakachulib'],
     author='Thorsten Bischler',
-    author_email='',
+    author_email='thorsten.bischler@uni-wuerzburg.de',
     description='Peak calling tool for CLIP-seq data',
     url='',
     install_requires=[
-        "pysam >= 0.8.3"
+        "biopython >= 1.66",
+        "matplotlib >= 1.5.1",
+        "pandas >= 0.17.1",
+        "pysam >= 0.9.0",
+        "bcbio-gff >= 0.6.2",
+        "statsmodels >= 0.7.0rc1",
+        "rpy2 >= 2.7.8"
     ],
     scripts=['bin/peakachu'],
     license='ISC License (ISCL)',
