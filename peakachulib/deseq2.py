55de8900b13bff579ea7b53ddeafcc3917515b3a
Thorsten Bischler <thorsten.bischler@uni-wuerzburg.de>
2016-06-02 19:14:05 +0200

    Fix problem that colnames of countData have to match with rownames of colData in DESeq2

--- peakachulib/deseq2.py
+++ peakachulib/deseq2.py
@@ -15,7 +15,7 @@ class RunDESeq2(object):
         self._ctr_lib_list = ctr_lib_list
         self._size_factors = size_factors
         self._pairwise_replicates = pairwise_replicates
-        
+
     def run_deseq2(self):
         self._count_df = np.round(self._count_df, decimals=0)
         self._count_df = self._count_df.astype(int)
@@ -26,7 +26,7 @@ class RunDESeq2(object):
             samples = list(range(1, len(self._exp_lib_list) + 1)) + list(
                 range(1, len(self._ctr_lib_list) + 1))
             colData = robjects.DataFrame({
-                    "conditions": robjects.StrVector(conds), 
+                    "conditions": robjects.StrVector(conds),
                     "samples": robjects.StrVector(samples)})
             design = Formula('~ samples + conditions')
         else:
@@ -34,7 +34,9 @@ class RunDESeq2(object):
                     {"conditions": robjects.StrVector(conds)})
             design = Formula('~ conditions')
         colData.rownames = libs
-        dds = r.DESeqDataSetFromMatrix(countData=self._count_df,
+        r_count_df = robjects.DataFrame(self._count_df)
+        r_count_df.colnames = robjects.rinterface.NULL
+        dds = r.DESeqDataSetFromMatrix(countData=r_count_df,
                                        colData=colData, design=design)
         if self._size_factors is None:
             dds = r.estimateSizeFactors(dds)
