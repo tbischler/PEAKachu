147d92a88e37f6ccd16c56b5e211962d498e12e3
Thorsten Bischler <thorsten.bischler@uni-wuerzburg.de>
2016-08-31 16:06:19 +0200

    Simplify method structure

--- peakachulib/library.py
+++ peakachulib/library.py
@@ -16,7 +16,7 @@ class Library(object):
         self.lib_name = splitext(basename(bam_file))[0]
         self.replicon_dict = replicon_dict
 
-    def _calc_window_expr(self):
+    def count_reads_for_windows(self):
         read_counter = ReadCounter(self.paired_end, self.max_insert_size,
                                    self.bam_file)
         for replicon in self.replicon_dict:
@@ -30,7 +30,7 @@ class Library(object):
                     strand] = window_counts
         read_counter.close_bam()
 
-    def _calc_peak_expr(self):
+    def count_reads_for_peaks(self):
         read_counter = ReadCounter(self.paired_end, self.max_insert_size,
                                    self.bam_file)
         for replicon in self.replicon_dict:
@@ -41,14 +41,8 @@ class Library(object):
             self.replicon_dict[replicon]["peak_counts"] = peak_counts
         read_counter.close_bam()
 
-    def count_reads_for_windows(self):
-        self._calc_window_expr()
-
     def merge_reads(self):
         bam_to_bed = BamToBed(self.paired_end, self.max_insert_size)
         for replicon, reads in bam_to_bed.generate_bed_format(self.bam_file):
             self.replicon_dict[replicon]["reads"] = pd.Series(reads)
         return self.replicon_dict  # it seems that a copy is returned!
-
-    def count_reads_for_peaks(self):
-        self._calc_peak_expr()
